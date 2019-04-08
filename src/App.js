a1b4b65007cdfb8857c75f41706981819c5e15f2
shelnutdan <shelnutdaniel@gmail.com>
2019-04-07 21:46:03 -0700

    Fourth commit: added error handling for forecast, city selection input field, and README.md for documentation

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
       isChecked:true,
       forecast:[],
       error: null,
-      zipcode:0
+      zipcode:0,
+      city:"",
+      err:true
 
     }
     this.handleChange=this.handleChange.bind(this);
@@ -23,49 +25,113 @@ class App extends Component {
   handleSubmit(event){
     event.preventDefault();
     const zipcode=this.state.zipcode;
+    const cityCountry=this.state.city;
     this.setState({ forecast: [] });
-    console.log(zipcode);
-    if(this.state.oneDay){
-      fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&APPID=fc20bc99a527278382bcc31e4b774425`).then(response=>response.json()).then(
-        (data)=>{
-          console.log(data)
-          this.setState(prevState =>{
-            return (
-              {
-                forecast: [...prevState.forecast,data]
-              }
-            )
-          })
-          /*
-          this.setState({
-          temperature: (data.main.temp-273.15).toFixed(1),
-          location: data.name,
-          weather: data.weather[0].main,
-          icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`*/
-          console.log(this.state.forecast)
-    }).catch( error=> console.log('Parsing error',error))
+    console.log(zipcode.length);
+    console.log(cityCountry);
+    if(zipcode=="" && cityCountry==""){
+      alert("please enter in a city or zipcode")
     }
+    else if(zipcode.length==5 && cityCountry!=""){
+      alert("please enter in only a city or a zipcode")
+    }
+    else if(zipcode.length <5 && zipcode.length>0){
+      alert('Enter a valid zipcode for the US')
+    }
+    else if(zipcode.length==5 && cityCountry==""){
+      if(this.state.oneDay){
+       fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&APPID=fc20bc99a527278382bcc31e4b774425`).then(
+         response=>response.json()).then(
+         (data)=>{
+           console.log(this.state.err)
+           this.setState({err:true})
+           this.setState(prevState =>{
+             return (
+               {
+                 forecast: [...prevState.forecast,data]
+               }
+             )
+           })
 
-    else if( this.state.fiveDay){
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},us&APPID=fc20bc99a527278382bcc31e4b774425`).then(response=>response.json()).then(
-        (data)=>{
-        let list=data.list
+     }).catch( error=> {
+       this.setState({err:false})
 
-        for (let i=0;i<list.length;i+=8){
-          this.setState(prevState =>{
-            return (
-              {
-                forecast: [...prevState.forecast,data.list[i]]
-              }
-            )
-          })
-          console.log(list[i])
-        }
+     })
+     }
 
-        }
-      ).catch( error=> console.log('Parsing error',error))
-    }else{
-      console.log('Select a forecast option')
+       else if( this.state.fiveDay){
+         fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},us&APPID=fc20bc99a527278382bcc31e4b774425`).then(response=>response.json()).then(
+           (data)=>{
+             this.setState({err:true})
+           let list=data.list
+
+           for (let i=0;i<list.length;i+=8){
+             this.setState(prevState =>{
+               return (
+                 {
+                   forecast: [...prevState.forecast,data.list[i]],
+
+                 }
+               )
+             })
+           }
+           }
+         ).catch( error=> {
+           this.setState({err:false})
+
+         })
+       }else{
+         alert("Please select a forecast option")
+       }
+    }
+    else if( zipcode.length==0 && cityCountry!="" && cityCountry.includes(",")){
+      console.log(cityCountry.split(",")[0].toLowerCase(),cityCountry.split(",")[1].toLowerCase())
+      let city=cityCountry.split(",")[0].toLowerCase();
+      let country=cityCountry.split(",")[1].toLowerCase()
+        if(this.state.oneDay){
+         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=fc20bc99a527278382bcc31e4b774425`).then(response=>response.json()).then(
+           (data)=>{
+             console.log(data)
+             this.setState({err:true})
+             this.setState(prevState =>{
+               return (
+                 {
+                   forecast: [...prevState.forecast,data]
+                 }
+               )
+             })
+       }).catch( error=>{
+         this.setState({err:false})
+
+       })
+       }
+     else if(this.state.fiveDay){
+
+         fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=fc20bc99a527278382bcc31e4b774425`).then(response=>response.json()).then(
+           (data)=>{
+             console.log(data)
+             this.setState({err:true})
+           let list=data.list
+           for (let i=0;i<list.length;i+=8){
+             this.setState(prevState =>{
+               return (
+                 {
+                   forecast: [...prevState.forecast,data.list[i]]
+                 }
+               )
+             })
+           }
+           }
+         ).catch( error=>{
+           this.setState({err:false})
+
+         }
+           )
+
+     }else{
+       alert("Please select a forecast option")
+     }
+
     }
 
 
@@ -78,7 +144,7 @@ class App extends Component {
  }
 handledate(date){
   let d=new Date(date*1000);
-  //let ds=d.toString("MMM dd yy");
+
   let ds= (d.getMonth()+1)+"/"+ d.getDate()+"/"+d.getFullYear()
   return ds;
 }
@@ -107,7 +173,7 @@ handledate(date){
  }
 
   render() {
-      var testRender=[{id:1,test:this.state.date},{id:2,test:200}]
+
       return (
         <div className="App">
         <header>
@@ -115,12 +181,17 @@ handledate(date){
           <p>Just enter in your locoation what type of forecast you want</p></header>
           <div className='weatherInputs'>
             <form onSubmit={this.handleSubmit}>
-              <label>Zip-Code:
+              <label> Zip-Code:
                 <input type='number' name='zipcode' value={this.state.value} onChange={this.handleChange}/>
               </label>
               <br/>
-
-              <input type='checkbox' name='forecast_length' value=' 1 day' onClick={this.todaysForecast} />Todays forecast
+              OR
+              <br/>
+              <label> City,Country:
+                <input type='text' name='city' placeholder="London,UK" value={this.state.value} onChange={this.handleChange}/>
+              </label>
+              <br/>
+              <input type='checkbox' name='forecast_length' value=' 1 day' onClick={this.todaysForecast} />Todays forecast<br/>
               <input type='checkbox' name='forecast_length' value=' 5 day' onClick={this.fiveDayForecast}/>Next Five Days<br/>
               <label>
               Choose Temperature unit:
@@ -130,7 +201,7 @@ handledate(date){
               <input type='submit' value='Submit'/>
             </form>
           </div>
-          <div className='weatherDisplay'>
+          {this.state.err? <div className='weatherDisplay'>
           {this.state.forecast.map( e =>
             <div className='individualDays'>
             <img
@@ -144,7 +215,7 @@ handledate(date){
               <p>Weather: {e.weather[0].main}</p>
             </div>
           )}
-          </div>
+          </div>:<div className='weatherDisplay'>Data for location could not be found, please enter in a valid location</div>}
         </div>
       );
 
